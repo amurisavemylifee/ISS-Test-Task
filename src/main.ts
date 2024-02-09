7f52ff49571262d6188475a91923bc35cfce31a6
Taras Shashurin <taras.shashurin.work@gmail.com>
2024-02-10 00:25:44 +0300

    fonts added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import "./style.scss";
+import "./style/index.scss";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import App from "./App.vue";
