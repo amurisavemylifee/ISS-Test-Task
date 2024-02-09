51fc53209197a2e2c0a0234c2acbdc61aa6ba483
Taras Shashurin <taras.shashurin.work@gmail.com>
2024-02-10 01:01:04 +0300

    pinia added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from "vue";
-import "./style/index.scss";
+import { createPinia } from "pinia";
 import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
-import App from "./App.vue";
+
 import router from "./router";
 
-createApp(App).use(ElementPlus).use(router).mount("#app");
+import "./style/index.scss";
+import "element-plus/dist/index.css";
+
+import App from "./App.vue";
+
+createApp(App).use(ElementPlus).use(router).use(createPinia()).mount("#app");
